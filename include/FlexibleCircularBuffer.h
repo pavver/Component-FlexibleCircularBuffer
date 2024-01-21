60ad7178e4d38588339865974936e91f18ff4326
pavver <pavvers1@gmail.com>
2024-01-21 14:59:21 +0200

    WriteToLastLine

--- include/FlexibleCircularBuffer.h
+++ include/FlexibleCircularBuffer.h
@@ -188,16 +188,7 @@ public:
     if (_indexFirstLine == -1)
       _indexFirstLine = nextIndex;
     else
-    {
-      BufferLineMarker line = lines[_indexFirstLine];
-      bool InIntersection = line.inIntersection(newLine);
-      while (InIntersection)
-      {
-        _indexFirstLine = getNextIndex(_indexFirstLine);
-        line = lines[_indexFirstLine];
-        InIntersection = line.inIntersection(newLine);
-      }
-    }
+      FixIntersection(newLine);
 
     // Set the new line as the last line.
     _indexLastLine = nextIndex;
@@ -207,6 +198,42 @@ public:
 
     // Return the id of the new line.
     return newLine.id;
+  }
+
+  /// @brief add data to last line
+  /// @param id id of the last line of data, in order to make sure that the data will be written exactly in the required line, if a new line was created and you try to write in the old line, 0 will be returned
+  /// @param data data
+  /// @param length data length
+  /// @return id of the created line. 0 if error
+  uint32_t WriteToLastLine(uint32_t id, const BuffT *data, uint16_t length)
+  {
+    if (_indexLastLine == -1 || id != lines[_indexLastLine].id ||
+        calculateLineLength(lines[_indexLastLine]) > _bufferSize / 2)
+      return 0;
+
+    // if the data fits into the buffer without fragmentation, then we simply write it to the buffer
+    if ((lines[_indexLastLine].endIndex + length + 1) < _bufferSize)
+    {
+      memcpy(buff + lines[_indexLastLine].endIndex + 1, data, length * sizeof(BuffT));
+      lines[_indexLastLine].endIndex = lines[_indexLastLine].endIndex + length;
+    }
+    else
+    {
+      // Otherwise, we need to split the line.
+      memcpy(buff + lines[_indexLastLine].endIndex + 1, data,
+             (_bufferSize - lines[_indexLastLine].endIndex - 1) * sizeof(BuffT));
+      lines[_indexLastLine].endIndex = _bufferSize - lines[_indexLastLine].endIndex - 1;
+      data += lines[_indexLastLine].endIndex;
+      length -= lines[_indexLastLine].endIndex;
+
+      // Write the rest of the data to a new line.
+      memcpy(buff, data, length * sizeof(BuffT));
+      lines[_indexLastLine].endIndex = length - 1;
+    }
+
+    FixIntersection(lines[_indexLastLine]);
+
+    return id;
   }
 
   /// @brief Read the first buffer line
@@ -306,9 +333,18 @@ private:
     return (index + _maxLines - 1) % _maxLines;
   };
 
-  /// @brief Creates a BufferLine with a pointer to the data in the buffer, or if the data is fragmented, allocates memory and copies the fragments there
-  /// @param index
-  /// @return
+  void FixIntersection(BufferLineMarker &newLine)
+  {
+    BufferLineMarker *line = &lines[_indexFirstLine];
+    bool InIntersection = line->inIntersection(newLine);
+    while (InIntersection)
+    {
+      _indexFirstLine = getNextIndex(_indexFirstLine);
+      line = &lines[_indexFirstLine];
+      InIntersection = line->inIntersection(newLine);
+    }
+  }
+
   uint16_t calculateLineLength(BufferLineMarker &line)
   {
     if (line.startIndex < line.endIndex)
@@ -367,3 +403,40 @@ private:
 #endif
   }
 };
+
+/// @brief add data to last line. if char data type then remove \0 simbol
+/// @param id id of the last line of data, in order to make sure that the data will be written exactly in the required line, if a new line was created and you try to write in the old line, 0 will be returned
+/// @param data data
+/// @param length data length
+/// @return id of the created line. 0 if error
+template <>
+uint32_t FlexibleCircularBuffer<char>::WriteToLastLine(uint32_t id, const char *data, uint16_t length)
+{
+  if (_indexLastLine == -1 || id != lines[_indexLastLine].id ||
+      calculateLineLength(lines[_indexLastLine]) + length > _bufferSize / 2)
+    return 0;
+
+  // if the data fits into the buffer without fragmentation, then we simply write it to the buffer
+  if ((lines[_indexLastLine].endIndex + length) < _bufferSize)
+  {
+    memcpy(buff + lines[_indexLastLine].endIndex, data, length * sizeof(char));
+    lines[_indexLastLine].endIndex = lines[_indexLastLine].endIndex + length - 1;
+  }
+  else
+  {
+    // Otherwise, we need to split the line.
+    memcpy(buff + lines[_indexLastLine].endIndex, data,
+           (_bufferSize - lines[_indexLastLine].endIndex - 1) * sizeof(char));
+    lines[_indexLastLine].endIndex = _bufferSize - lines[_indexLastLine].endIndex - 2;
+    data += lines[_indexLastLine].endIndex;
+    length -= lines[_indexLastLine].endIndex + 1;
+
+    // Write the rest of the data to a new line.
+    memcpy(buff, data, length * sizeof(char));
+    lines[_indexLastLine].endIndex = length - 1;
+  }
+
+  FixIntersection(lines[_indexLastLine]);
+
+  return id;
+}
